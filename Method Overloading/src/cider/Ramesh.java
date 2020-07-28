01f7235e163ebb642276f6f886f692c89af8eda1
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-28 18:16:16 -0500

    Method Overloading completed

---
+++ Method Overloading/src/cider/Ramesh.java
@@ -0,0 +1,30 @@
+package cider;
+
+public class Ramesh
+{
+   public void function1()
+   {
+	   System.out.println("hi");
+   }
+    public void function1(int i)
+    {
+    	System.out.println(i);
+    }
+    public void function1(int j,String h)
+    {
+    	System.out.println(j);
+    	System.out.println(h);
+    }
+     public String function1(String h)
+     {  
+    	 return h;
+     }
+     public void function1(float k,String h,int v)
+     {
+    	 System.out.println(k);
+    	 System.out.println(h);
+    	 System.out.println(v);
+     }
+     
+}
+
---
+++ Method Overloading/src/cider/Suresh.java
@@ -0,0 +1,18 @@
+package cider;
+
+public class Suresh
+{
+
+	public static void main(String[] args)
+	{
+    Ramesh v=new Ramesh();
+          v.function1();
+          v.function1(4);
+          v.function1(8,"dolly");
+      String x=    v.function1("bittu");
+       System.out.println(x);
+            v.function1(3455.5f,"dinku",6);
+            
+	}
+
+}
