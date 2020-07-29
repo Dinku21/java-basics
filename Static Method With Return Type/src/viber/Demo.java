08a1649d068e51f50374474d235f80851da9f1a6
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 17:23:12 -0500

    Static Methods With Return Type completed

---
+++ Static Method With Return Type/src/viber/Demo.java
@@ -0,0 +1,19 @@
+package viber;
+
+public class Demo 
+{
+  private static int q=7;
+  private static float p=678f;
+  
+  public static int mode1()
+  {
+	  return q;
+  }
+  public static float mode2()
+  {
+	  return p;
+  }
+ 
+ 
+ 
+}
---
+++ Static Method With Return Type/src/viber/Sample.java
@@ -0,0 +1,18 @@
+package viber;
+
+public class Sample
+{
+
+	public static void main(String[] args)
+	{
+	int k=	Demo.mode1();
+	System.out.println(k);
+	float v=Demo.mode2();
+	System.out.println(v);
+	
+	     
+		
+
+	}
+
+}
