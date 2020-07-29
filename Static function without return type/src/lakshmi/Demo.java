412c96e8eda169a3c438697bfdca1fccc5c7e439
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 17:18:55 -0500

    Static Functions Without Return Type completed

---
+++ Static function without return type/src/lakshmi/Demo.java
@@ -0,0 +1,16 @@
+package lakshmi;
+
+public class Demo 
+{
+  private static int f=3;
+  private static String name="dinku";
+  
+   public static void net1()
+   {
+	   System.out.println(f);
+   }
+   public static void net2()
+   {
+	   System.out.println(name);
+   }
+}
---
+++ Static function without return type/src/lakshmi/Sample.java
@@ -0,0 +1,13 @@
+package lakshmi;
+
+public class Sample
+{
+
+	public static void main(String[] args)
+	{
+		Demo.net1();
+		Demo.net2();
+
+	}
+
+}
