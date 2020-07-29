f6132e0ce50bba1df33bcdc5126142d21ba1f5c3
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 17:06:07 -0500

    Primitive Data Types Completed

---
+++ Primitive Data Types/src/Vrishank/One.java
@@ -0,0 +1,33 @@
+package Vrishank;
+
+public class One 
+
+{ 
+	private int id=1;
+   private int e=7;
+   private int u=10;
+   
+   private String name="chinna";
+   private String valve="chitti";
+   private String number="dolly";
+   
+   private float g=11f;
+   private float y=0.2f;
+   private float salary=2.6f;
+   
+   private double h=234d;
+   private double k=678890d;
+   private double budget=223110d;
+   
+   private char mahesh='t';
+   private char t='y';
+   private char naresh ='5';
+   
+   private boolean kite=true;
+   private boolean row=false;
+   private boolean date=true;
+   
+   
+   
+   
+}
