f403678574b032ca355ebed3caca03de0ad374d7
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 17:14:03 -0500

    Static And Non Satic Functions With Return Type

---
+++ Static And Non Static Functions With Return Type/src/rider/Demo.java
@@ -0,0 +1,26 @@
+package rider;
+
+public class Demo 
+{
+  private static int y=2;
+  private static String j="dinku";
+  private int w=8;
+  private float g=76432f;
+  
+  public static int net1()
+  {
+	  return y;
+  }
+  public static String net2()
+  {
+	  return j;
+  }
+  public int net3()
+  {
+	  return w;
+  }
+  public float net4()
+  {
+	  return g;
+  }
+}
---
+++ Static And Non Static Functions With Return Type/src/rider/Sample.java
@@ -0,0 +1,20 @@
+package rider;
+
+public class Sample {
+
+	public static void main(String[] args)
+	{
+       int z=Demo.net1();
+       System.out.println(z);
+       String c=Demo.net2();
+       System.out.println(c);
+       Demo B=new Demo();
+       int q=B.net3();
+       System.out.println(q);
+       float t=B.net4();
+       System.out.println(t);
+       
+
+	}
+
+}
